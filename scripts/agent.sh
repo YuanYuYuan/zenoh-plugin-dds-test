6de50148edb57dbb2e4ea4e7332ed0d14ad8ae75
yuanyuyuan <az6980522@gmail.com>
2023-05-08 00:25:27 +0800

    Update scripts

--- scripts/agent.sh
+++ scripts/agent.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-# bash
+# export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+# source /opt/ros/humble/setup.bash
+# source /opt/workspace/install/setup.bash
+# cd /opt/workspace
+# ros2 run pubsub pub
+
 # sleep $[ ( $RANDOM % 5 )  + 1 ]s
-/opt/zenoh-plugin-dds/target/release/zenoh-bridge-dds --mode peer --connect tcp/192.168.1.2:7447 --no-multicast-scouting
+# /opt/zenoh-plugin-dds/target/release/zenoh-bridge-dds --mode peer --connect tcp/192.168.1.2:7447 --no-multicast-scouting
+
+/opt/zenoh-plugin-dds/target/release/zenoh-bridge-dds \
+  --mode peer \
+  --connect tcp/127.0.0.1:7447 \
+  --no-multicast-scouting
--- scripts/core.sh
+++ scripts/core.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+# source /opt/ros/humble/setup.bash
+# source /opt/workspace/install/setup.bash
+# cd /opt/workspace
+# ros2 run pubsub sub
+
 # bash
-/opt/zenoh-plugin-dds/target/release/zenoh-bridge-dds --mode peer --listen tcp/192.168.1.2:7447 --no-multicast-scouting
+# /opt/zenoh-plugin-dds/target/release/zenoh-bridge-dds --mode peer --listen tcp/192.168.1.2:7447 --no-multicast-scouting
+/opt/zenoh-plugin-dds/target/release/zenoh-bridge-dds --mode peer --listen tcp/127.0.0.1:7447 --no-multicast-scouting
